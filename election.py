f7d9c97daf46c57baa27d933eed7c93c0c367de0
gtienle <christinele@Christines-MacBook-Air.local>
2017-05-30 17:06:10 +0200

    update

--- election.py
+++ election.py
@@ -1,7 +1,14 @@
+#import libraries
+import csv
+
 def data(x):
 	#Load Data, delete non-essential columns
-	election = pd.read_csv("/Users/christinele/Desktop/SoSe17/DBS/Projekt/american-election-tweets.csv", index_col=0)
-	del election['in_reply_to_screen_name']
-	del election['is_quote_status']
-	del election['source_url']
-	del election['truncated']
+	election_new = pd.read_csv("/Users/christinele/Desktop/SoSe17/DBS/Projekt/american-election-tweets.csv", index_col=0)
+	del election_new['in_reply_to_screen_name']
+	del election_new['is_quote_status']
+	del election_new['source_url']
+	del election_new['truncated']
+print election_new.shape
+election_new.head()
+
+election_new.to_csv('/Users/christinele/Desktop/SoSe17/DBS/Projekt/election_new.csv', index=False)
