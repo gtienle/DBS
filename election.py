9ecb6015bbc6ed9689859345b8ccee4a20347c51
gtienle <christinele@Christines-Air.fritz.box>
2017-06-01 20:45:53 +0200

    bla

--- election.py
+++ election.py
@@ -1,9 +1,10 @@
 #import libraries
 import csv
+import pandas as pd
 
 def data(x):
 	#Load Data, delete non-essential columns
-	election_new = pd.read_csv("/Users/christinele/Desktop/SoSe17/DBS/Projekt/american-election-tweets.csv", index_col=0)
+	election_new = pd.read_csv("/Users/christinele/Desktop/SoSe17/DBS/DBS_Project/american-election-tweets.csv", index_col=0)
 	del election_new['in_reply_to_screen_name']
 	del election_new['is_quote_status']
 	del election_new['source_url']
@@ -11,4 +12,4 @@ def data(x):
 print election_new.shape
 election_new.head()
 
-election_new.to_csv('/Users/christinele/Desktop/SoSe17/DBS/Projekt/election_new.csv', index=False)
+#election_new.to_csv('/Users/christinele/Desktop/SoSe17/DBS/Projekt/election_new.csv', index=False)
